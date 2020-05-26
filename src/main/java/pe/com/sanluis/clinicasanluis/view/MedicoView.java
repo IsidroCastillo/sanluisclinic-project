74c4b617f5ae5db8c482ef5935ea79be4fdcec44
Isidro Castillo Muñoz <iscastillo05@hotmail.com>
2020-05-25 20:18:00 -0500

    changes in reports

--- src/main/java/pe/com/sanluis/clinicasanluis/view/MedicoView.java
+++ src/main/java/pe/com/sanluis/clinicasanluis/view/MedicoView.java
@@ -1,6 +1,7 @@
 package pe.com.sanluis.clinicasanluis.view;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,8 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRPptxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import pe.com.sanluis.clinicasanluis.entity.Especialidad;
 import pe.com.sanluis.clinicasanluis.entity.Horario;
 import pe.com.sanluis.clinicasanluis.entity.Medico;
@@ -50,7 +60,7 @@ public class MedicoView implements Serializable {
 		medicos = medicoRepocitory.findAll();
 		return medicos;
 	}
-	
+
 	public void registrar() {
 		boolean exits = medicoRepocitory.existsById(medico.getIdMedico());
 		Especialidad esp = especialidadRepocitory.findById(medico.getEspecialidad().getIdEspecialidad()).get();
@@ -103,6 +113,23 @@ public class MedicoView implements Serializable {
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 
+	public void exportarPDF(ActionEvent actionEvent) throws JRException, IOException {
+		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/medicos.jasper"));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(), null,
+				new JRBeanCollectionDataSource(this.getMedicosList()));
+
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext()
+				.getResponse();
+		response.addHeader("Content-disposition", "attachment; filename=jsfReporte.pdf");
+		ServletOutputStream stream = response.getOutputStream();
+
+		JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+		stream.flush();
+		stream.close();
+
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
 	public void verPDF(ActionEvent actionEvent) throws Exception {
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/medicos.jasper"));
 
@@ -117,6 +144,68 @@ public class MedicoView implements Serializable {
 		outStream.flush();
 		outStream.close();
 
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+//	pageHeader
+
+	public void exportarExcel(ActionEvent actionEvent) throws JRException, IOException {
+		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/medicos.jasper"));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(), null,
+				new JRBeanCollectionDataSource(this.getMedicosList()));
+
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext()
+				.getResponse();
+		response.addHeader("Content-disposition", "attachment; filename=jsfReporte.xls");
+		ServletOutputStream outStream = response.getOutputStream();
+
+		JRXlsExporter exporter = new JRXlsExporter();
+
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outStream));
+		exporter.exportReport();
+
+		outStream.flush();
+		outStream.close();
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
+	public void exportarDOC(ActionEvent actionEvent) throws JRException, IOException {
+		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/medicos.jasper"));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(), null,
+				new JRBeanCollectionDataSource(this.getMedicosList()));
+
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext()
+				.getResponse();
+		response.addHeader("Content-disposition", "attachment; filename=jsfReporte.doc");
+		ServletOutputStream outStream = response.getOutputStream();
+
+		JRDocxExporter exporter = new JRDocxExporter();
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outStream));
+		exporter.exportReport();
+
+		outStream.flush();
+		outStream.close();
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
+	public void exportarPPT(ActionEvent actionEvent) throws JRException, IOException {
+		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/medicos.jasper"));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(), null,
+				new JRBeanCollectionDataSource(this.getMedicosList()));
+
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext()
+				.getResponse();
+		response.addHeader("Content-disposition", "attachment; filename=jsfReporte.ppt");
+		ServletOutputStream outStream = response.getOutputStream();
+
+		JRPptxExporter exporter = new JRPptxExporter();
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outStream));
+		exporter.exportReport();
+
+		outStream.flush();
+		outStream.close();
 		FacesContext.getCurrentInstance().responseComplete();
 	}
 }
